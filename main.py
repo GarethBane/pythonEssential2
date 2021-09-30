e4778d62646a4fa42f97f9790ef1a43fdb7ee721
Phil Benford <phillipbenford@gmail.com>
2021-09-30 22:54:58 +0100

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+value = "sdadd"
+valid = ["1", "2", "3"]
+
+
+# if value[0] == valid[0] or valid[1] or valid[2]:
+#     print("valid")
+# else:
+#     print("invalid")
+
+var = "3"
+bar = "7"
+
+print(var and bar == valid[0] or valid[1])
