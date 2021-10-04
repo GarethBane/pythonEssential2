ab56027357d814cb8abc0b31df6e383828e72072
Phil Benford <phillipbenford@gmail.com>
2021-10-04 14:51:46 +0100

    commit

--- module2Labs.py
+++ module2Labs.py
@@ -2,7 +2,6 @@
 
 # LAB: 2.3.1.18 Your own split
 # write your own function, which behaves almost exactly like the original split() method
-
 def mysplit(strng):
     loop = True
     list = []
@@ -23,3 +22,46 @@ print(mysplit("To be or not to be,that is the question"))
 print(mysplit("   "))
 print(mysplit(" abc "))
 print(mysplit(""))
+
+# LAB: 2.4.1.6 A LED Display
+# This blew my mind and im still trying to work it out.
+digits = [ '1111110',  	# 0
+	   '0110000',	# 1
+	   '1101101',	# 2
+	   '1111001',	# 3
+	   '0110011',	# 4
+	   '1011011',	# 5
+	   '1011111',	# 6
+	   '1110000',	# 7
+	   '1111111',	# 8
+	   '1111011',	# 9
+	   ]
+
+def print_number(num):
+	global digits
+	digs = str(num)
+	lines = [ '' for lin in range(5) ]
+	for d in digs:
+		segs = [ [' ',' ',' '] for lin in range(5) ]
+		ptrn = digits[ord(d) - ord('0')]
+		if ptrn[0] == '1':
+			segs[0][0] = segs[0][1] = segs[0][2] = '#'
+		if ptrn[1] == '1':
+			segs[0][2] = segs[1][2] = segs[2][2] = '#'
+		if ptrn[2] == '1':
+			segs[2][2] = segs[3][2] = segs[4][2] = '#'
+		if ptrn[3] == '1':
+			segs[4][0] = segs[4][1] = segs[4][2] = '#'
+		if ptrn[4] == '1':
+			segs[2][0] = segs[3][0] = segs[4][0] = '#'
+		if ptrn[5] == '1':
+			segs[0][0] = segs[1][0] = segs[2][0] = '#'
+		if ptrn[6] == '1':
+			segs[2][0] = segs[2][1] = segs[2][2] = '#'
+		for lin in range(5):
+			lines[lin] += ''.join(segs[lin]) + ' '
+	for lin in lines:
+		print(lin)
+
+print_number(int(input("Enter the number you wish to display: ")))
+
