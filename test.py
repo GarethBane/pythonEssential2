751eaed206d081c7a8ee48d725a34f714dbf3492
Phil Benford <phillipbenford@gmail.com>
2021-10-20 12:44:09 +0100

    commit

---
+++ test.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+name_of_city = input("What's the name of the city you grew up in? ")
+name_of_pet = input("What's the name of your first pet? ")
+print("Your band name could be", name_of_city, name_of_pet)
+
